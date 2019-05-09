f0efb26c40cd7c4a522d62c07788e8f7e25d9c4a
garden <khjvvv7@gmail.com>
2019-05-09 20:15:28 +0900

    add delete & edit func

--- BLOG/myproject/blog/views.py
+++ BLOG/myproject/blog/views.py
@@ -3,8 +3,6 @@ from django.utils import timezone
 from .models import Blog
 from .models import Portfolio
 
-
-
 # Create your views here.
 def home(request):
     blogs = Blog.objects
@@ -31,3 +29,21 @@ def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
 
     return render(request,'detail.html',{'blog' : blog_detail})
+
+def delete(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    blog.delete()
+    return redirect('/')
+
+def edit(request, blog_id):
+    blog_edit =Blog.objects.get(id=blog_id)
+    return render(request, 'edit.html',{'blog' : blog_edit})
+
+def update(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+
+    return redirect('/blog/'+ str(blog_id))
